4ea2b2e1975a658219d8b9eb010ff19527197829
greenelk99 <152878864+greenelk99@users.noreply.github.com>
2023-12-22 18:07:54 +0100

    Added presents

---
+++ ChristmasJam/ChristmasJam/Assets/Scripts/Present.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Present : MonoBehaviour
+{
+    [SerializeField] float maxVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.relativeVelocity.magnitude > maxVelocity)
+        {
+            Invoke("Die", 0.1f);
+        }
+    }
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
